207940755a62888340936571978da5a6ed85fbad
Emmanuel Raviart <emmanuel@raviart.com>
2022-08-26 18:11:52 +0200

    Sort natures.

--- packages/tisseuse/src/routes/api/textes/natures/+server.ts
+++ packages/tisseuse/src/routes/api/textes/natures/+server.ts
@@ -5,7 +5,9 @@ import type { RequestHandler } from "./$types"
 export const GET: RequestHandler = async ({ url }) => {
   const natures = (
     await db<{ nature: string }[]>`
-      SELECT distinct nature FROM texte_version
+      SELECT distinct nature
+      FROM texte_version
+      ORDER BY nature
     `
   ).map(({ nature }) => nature)
   return new Response(

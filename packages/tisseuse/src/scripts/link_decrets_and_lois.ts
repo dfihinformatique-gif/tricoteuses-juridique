ffa78934e19f90a2cdb262ffa156315fc02a2dfb
Emmanuel Raviart <emmanuel@raviart.com>
2023-03-04 09:37:27 +0100

    WIP: Add link to lois from décrets

---
+++ packages/tisseuse/src/scripts/link_decrets_and_lois.ts
@@ -0,0 +1,39 @@
+import metslesliens from "metslesliens"
+import sade from "sade"
+
+import type { JorfTexteVersion } from "$lib"
+import { db } from "$lib/server/database"
+
+async function linkDecretsToLois(): Promise<void> {
+  for (const texteVersion of (
+    await db<{ data: JorfTexteVersion }[]>`
+      SELECT data
+      FROM texte_version
+      WHERE
+        data -> 'META' -> 'META_COMMUN' ->> 'NATURE' = 'DECRET'
+        AND data -> 'META' -> 'META_COMMUN' ->> 'ORIGINE' = 'JORF'
+    `
+  ).map(({ data }) => data)) {
+    const notice = texteVersion.NOTICE?.CONTENU
+    if (notice === undefined) {
+      continue
+    }
+    console.log(
+      "\n################################################################################",
+    )
+    console.log(notice)
+    console.log(
+      "--------------------------------------------------------------------------------",
+    )
+    const links = metslesliens.getLinks(notice /* , metslesliens.getParser() */)
+    console.log(JSON.stringify(links, null, 2))
+  }
+}
+
+sade("link_decrets_and_lois", true)
+  .describe("Link decrets to lois in database")
+  .action(async () => {
+    await linkDecretsToLois()
+    process.exit(0)
+  })
+  .parse(process.argv)

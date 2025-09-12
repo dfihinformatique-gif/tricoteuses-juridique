4009786275dd41218619f1ab5d8d323da0804099
Emmanuel Raviart <emmanuel@raviart.com>
2025-09-12 22:54:20 +0200

    feat: export most tested parsers & some tools

--- packages/tisseuse/src/lib/index.ts
+++ packages/tisseuse/src/lib/index.ts
@@ -1,4 +1,138 @@
+export { formatLongDate } from "./dates.js"
+export {
+  iterCardinalNumeralFormsFromNumber,
+  iterLatinMultiplicativeAdverbsFromNumber,
+  iterOrdinalNumeralFormsFromNumber,
+  numberFromRomanNumeral,
+  ordinalNumeralFromNumber,
+  romanNumeralFromNumber,
+} from "./numbers.js"
+export { action } from "./text_parsers/actions.js"
+export {
+  article,
+  articles,
+  definitionArticle,
+  designationArticle,
+  listeArticles,
+  nomArticle,
+  nomSpecialArticle,
+} from "./text_parsers/articles.js"
+export {
+  compoundReferencesSeparators,
+  divisionTypes,
+  europeanLawNatures,
+  frenchLawNatures,
+  internationalLawNatures,
+  isTextAstAtomicReference,
+  isTextAstDivision,
+  isTextAstPortion,
+  lawNatures,
+  localizationAdverbs,
+  portionTypes,
+  type CompoundReferencesSeparator,
+  type DivisionType,
+  type EuropeanLawNature,
+  type FrenchLawNature,
+  type InternationalLawNature,
+  type LawNature,
+  type LocalizationAdverb,
+  type PortionType,
+  type TextAst,
+  type TextAstAction,
+  type TextAstArticle,
+  type TextAstAtomicReference,
+  type TextAstBoundedInterval,
+  type TextAstCitation,
+  type TextAstCompoundReference,
+  type TextAstConseilConstitutionnelDecision,
+  type TextAstCountedInterval,
+  type TextAstDivision,
+  type TextAstEnumeration,
+  type TextAstExclusion,
+  type TextAstIncompleteHeader,
+  type TextAstLocalization,
+  type TextAstNumber,
+  type TextAstParentChild,
+  type TextAstPortion,
+  type TextAstPosition,
+  type TextAstReference,
+  type TextAstReferenceAndAction,
+  type TextAstText,
+  type TextAstTextIdentification,
+  type TextAstTextInfos,
+  type TextInfosByWordsTree,
+} from "./text_parsers/ast.js"
+export {
+  citation,
+  citationLigne,
+  citationSimple,
+} from "./text_parsers/citations.js"
+export { date, duDate } from "./text_parsers/dates.js"
+export {
+  definitionDivision,
+  designationDivision,
+  division,
+  divisions,
+  natureDivisionSingulier,
+  numeroDivision,
+} from "./text_parsers/divisions.js"
+export {
+  addChildLeftToLastChild,
+  createEnumerationOrBoundedInterval,
+  createParentChildTreeFromReferences,
+  iterAtomicFirstParentReferences,
+  iterAtomicOrParentChildReferences,
+  iterAtomicReferences,
+  iterIncludedReferences,
+} from "./text_parsers/helpers.js"
+export {
+  adjectifNumeralOrdinalCourt,
+  adverbeMultiplicatifLatin,
+  nombreAsTextAstNumber,
+  nombreCardinal,
+  nombreRomainCardinal,
+  nombreRomainOrdinal,
+  nombreRomainOu0iAsTextAstNumber,
+} from "./text_parsers/numbers.js"
+export {
+  alternatives,
+  chain,
+  convert,
+  optional,
+  parseText,
+  regExp,
+  repeat,
+  TextParserContext,
+  variable,
+  wordsTree,
+} from "./text_parsers/parsers.js"
+export {
+  auPortion,
+  auxPortions,
+  numeroPortion,
+  portions,
+  unePortion,
+} from "./text_parsers/portions.js"
 export type { TextPosition } from "./text_parsers/positions.js"
+export {
+  ditPluriel,
+  ditSingulier,
+  introPluriel,
+  introSingulier,
+  liaisonPluriel,
+  liaisonSingulier,
+} from "./text_parsers/prepositions.js"
+export {
+  reference,
+  uniteBasePreciseeSingulier,
+  uniteBaseSingulier,
+} from "./text_parsers/references.js"
+export {
+  adverbeRelatif,
+  espaceAdverbeRelatif,
+  relatifPlurielPrepose,
+  relatifSingulierPrepose,
+} from "./text_parsers/relative_locations.js"
 export {
   convertHtmlElementsToText,
   decodeNamedHtmlEntities,
@@ -9,6 +143,16 @@ export {
   simplifyText,
   simplifyUnicodeCharacters,
 } from "./text_parsers/simplifiers.js"
+export {
+  identificationTexteEuropeen,
+  numeroEtOuDateTexteFrancais,
+  numeroTexteEuropeen,
+  numeroTexteFrancais,
+  texte,
+  texteEuropeen,
+  texteFrancais,
+  texteInternational,
+} from "./text_parsers/texts.js"
 export {
   chainTransformers,
   iterOriginalMergedPositionsFromTransformed,
@@ -23,3 +167,11 @@ export {
   type TransformerLeaf,
   type TransformerNode,
 } from "./text_parsers/transformers.js"
+export {
+  espace,
+  lettreAsciiMinuscule,
+  nonLettre,
+  numero,
+  virguleOuEspace,
+} from "./text_parsers/typography.js"
+export { escapeHtml } from "./strings.js"

f6beac8597b47d0b40f2679c618341b0e02060a0
Emmanuel Raviart <emmanuel@raviart.com>
2025-10-02 20:19:32 +0200

    feat: use shadcn-svelte for autocompleter

--- packages/chercheuse/src/routes/+page.svelte
+++ packages/chercheuse/src/routes/+page.svelte
@@ -1,30 +1,32 @@
 <script lang="ts">
+  import * as Command from "$lib/components/ui/command/index.js"
   import { urlPathFromId } from "$lib/urls.js"
 
   import { autocomplete } from "./autocompletion.remote.js"
 
   let q: string = $state("loi république numérique")
+  let suggestions = $derived(await autocomplete(q))
 </script>
 
 <h1 class="my-4 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
   Tricoteuses
 </h1>
 
-<input bind:value={q} />
-<ul>
-  <svelte:boundary>
-    {#each await autocomplete(q) as { autocompletion, distance, id }}
-      {@const urlPath = urlPathFromId(id)}
-      <li>
-        {#if urlPath === null}
-          {autocompletion}
-        {:else}
-          <a href={urlPath}>{autocompletion}</a>
-        {/if}
-      </li>
-    {/each}
-    {#snippet pending()}
-      <p>loading...</p>
-    {/snippet}
-  </svelte:boundary>
-</ul>
+<Command.Root shouldFilter={false}>
+  <Command.Input placeholder="Type a command or search..." bind:value={q} />
+  <Command.List>
+    <Command.Empty>No results found.</Command.Empty>
+    <Command.Group>
+      {#each suggestions as { autocompletion, distance, id }}
+        {@const urlPath = urlPathFromId(id)}
+        <Command.Item>
+          {#if urlPath === null}
+            {autocompletion}
+          {:else}
+            <a href={urlPath}>{autocompletion}</a>
+          {/if}
+        </Command.Item>
+      {/each}
+    </Command.Group>
+  </Command.List>
+</Command.Root>

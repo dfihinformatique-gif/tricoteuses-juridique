7feda74ff888be661129fb267fdb1aeab921a8db
Emmanuel Raviart <emmanuel@raviart.com>
2025-10-21 17:07:51 +0200

    feat: add autocompletion to URL search params

--- packages/exploratrice/src/routes/+page.svelte
+++ packages/exploratrice/src/routes/+page.svelte
@@ -1,10 +1,59 @@
 <script lang="ts">
+  import {
+    auditSetNullish,
+    auditString,
+    cleanAudit,
+    type Audit,
+  } from "@auditors/core"
+
+  import { pushState } from "$app/navigation"
+  import { auditQuerySingleton } from "$lib/auditors/queries"
   import * as Command from "$lib/components/ui/command/index.js"
   import { urlPathFromId } from "$lib/urls.js"
 
   import { autocomplete } from "./autocompletion.remote.js"
+  import { page } from "$app/state"
 
-  let q: string = $state("loi république numérique")
+  const auditQuery = (
+    audit: Audit,
+    query: URLSearchParams,
+  ): [{ q: string }, unknown] => {
+    if (query == null) {
+      return [query, null]
+    }
+    if (!(query instanceof URLSearchParams)) {
+      return audit.unexpectedType(query, "URLSearchParams")
+    }
+
+    const data: { [key: string]: string[] } = {}
+    for (const [key, value] of query.entries()) {
+      let values = data[key]
+      if (values === undefined) {
+        values = data[key] = []
+      }
+      values.push(value)
+    }
+    const errors: { [key: string]: unknown } = {}
+    const remainingKeys = new Set(Object.keys(data))
+
+    audit.attribute(
+      data,
+      "q",
+      true,
+      errors,
+      remainingKeys,
+      auditQuerySingleton(auditString, auditSetNullish("")),
+    )
+
+    return audit.reduceRemaining(
+      data,
+      errors,
+      remainingKeys,
+      auditSetNullish({}),
+    )
+  }
+
+  let { q } = $state(auditQuery(cleanAudit, page.url.searchParams)[0])
   let suggestions = $derived(await autocomplete(q))
 </script>
 
@@ -13,7 +62,16 @@
 </h1>
 
 <Command.Root shouldFilter={false}>
-  <Command.Input placeholder="Type a command or search..." bind:value={q} />
+  <Command.Input
+    placeholder="Nom de loi ou de projet de loi ou de JO…"
+    bind:value={
+      () => q,
+      (value) => {
+        q = value
+        pushState(`?q=${encodeURIComponent(q)}`, {})
+      }
+    }
+  />
   <Command.List>
     <Command.Empty>No results found.</Command.Empty>
     <Command.Group>

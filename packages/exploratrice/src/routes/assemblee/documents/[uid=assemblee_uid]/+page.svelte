0fbb14868af05d5f14747196f109accb47115648
Emmanuel Raviart <emmanuel@raviart.com>
2025-10-21 07:02:24 +0200

    fix: use @attach to avoid Svelte bug with shadowrootmode=

--- packages/exploratrice/src/routes/assemblee/documents/[uid=assemblee_uid]/+page.svelte
+++ packages/exploratrice/src/routes/assemblee/documents/[uid=assemblee_uid]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import EllipsisVerticalIcon from "@lucide/svelte/icons/ellipsis-vertical"
   import ExternalLinkIcon from "@lucide/svelte/icons/external-link"
+  import type { Attachment } from "svelte/attachments"
 
   import { Badge } from "$lib/components/ui/badge/index.js"
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js"
@@ -15,6 +16,15 @@
     document,
     /* documentFileInfos, */ /* documentFilesIndex, */ documentHtml,
   } = $derived(documentPageInfos)
+
+  const attachDocumentHtml: Attachment = (element) => {
+    const shadow = element.attachShadow({ mode: "open" })
+    shadow.innerHTML = documentHtml
+
+    return () => {
+      // Cleaning up
+    }
+  }
 </script>
 
 <h1 class="my-4 scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
@@ -39,8 +49,15 @@
   </DropdownMenu.Root>
 </div>
 
-<section>
+<!--
+Note: shadowrootmode="open" doesn't currently work in Svelte when rendered on the client.
+(but it works when rendered on the server).
+When bug https://github.com/sveltejs/svelte/issues/13271 is closed, remove the comment and
+delete the article with @attach function below.
+<article>
   <template shadowrootmode="open">
     {@html documentHtml}
   </template>
-</section>
+</article>
+-->
+<article {@attach attachDocumentHtml}></article>
